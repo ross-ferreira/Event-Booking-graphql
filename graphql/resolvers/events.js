190665fd775ca44012a79e7eeaf1fcfd2a4df240
papenwors01 <ross.p.ferreira@gmail.com>
2021-04-26 11:10:42 +0100

    refactored code, place resolvers into seperate files

---
+++ graphql/resolvers/events.js
@@ -0,0 +1,45 @@
+const Event = require("../../models/event");
+
+const {transformEvent, } = require("./merge");
+
+
+module.exports = {
+  // events here is linked to events schema
+  events: async () => {
+    try {
+      const events = await Event.find();
+      // mapping over events to extract out the data needed as mongo will respond with metadata within the request
+      return events.map((event) => {
+        return transformEvent(event);
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+  createEvent: async (args) => {
+    // this Event is from the models schema
+    const event = new Event({
+      title: args.eventInput.title,
+      description: args.eventInput.description,
+      price: +args.eventInput.price,
+      date: new Date(args.eventInput.date),
+      // creator needs to be store as an object id but mongoose can convert a string to an object id automatically
+      creator: "6081a58cbb176c4e653c557d",
+    });
+    let createdEvent;
+    // this "save" function is provided by mongoose - it will write the data into the mongodb db
+    try {
+      const result = await event.save();
+      createdEvent = transformEvent(result);
+      const creator = await User.findById("6081a58cbb176c4e653c557d");
+      if (!creator) {
+        throw new Error("User not found");
+      }
+      creator.createdEvents.push(event);
+      await creator.save();
+      return createdEvent;
+    } catch (err) {
+      throw err;
+    }
+  },
+};
---
+++ graphql/resolvers/merge.js
@@ -0,0 +1,67 @@
+const Event = require("../../models/event");
+const User = require("../../models/user");
+const { dateToString } = require("../../helpers/date");
+
+// in await it always returns so dont need to add return after await
+const events = async (eventIds) => {
+  try {
+    const events = await Event.find({
+      _id: { $in: eventIds },
+    });
+    events.map((event) => {
+      return transformEvent(event);
+    });
+    return events;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const singleEvent = async (eventId) => {
+  try {
+    const event = await Event.findById(eventId);
+    return transformEvent(event);
+  } catch (err) {
+    throw err;
+  }
+};
+
+const user = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    return {
+      ...user._doc,
+      _id: user.id,
+      // graphql will check to see if the value is a string/function and if function as in "createdEvents" it will call it
+      createdEvents: events.bind(this, user._doc.createdEvents),
+    };
+  } catch (err) {
+    throw err;
+  }
+};
+
+const transformEvent = (event) => {
+  return {
+    ...event._doc,
+    // need to convert id to a string from db
+    _id: event.id,
+    date: dateToString(event._doc.date),
+    creator: user.bind(this, event.creator),
+  };
+};
+const transformBooking = (booking) => {
+  return {
+    ...booking._doc,
+    _id: booking.id,
+    user: user.bind(this, booking._doc.user),
+    event: singleEvent.bind(this, booking._doc.event),
+    createdAt: dateToString(booking._doc.createdAt),
+    updatedAt: dateToString(booking._doc.updatedAt),
+  };
+};
+
+exports.transformEvent = transformEvent;
+exports.transformBooking = transformBooking;
+// exports.user = user;
+// exports.events = events;
+// exports.singleEvent = singleEvent;
